86c26ec49619116da90e8876274280fe0615782a
Nathan Davis Barrett <76917233+NathanDavisBarrett@users.noreply.github.com>
2024-09-10 06:54:04 -0400

    Fixed import *

--- PyomoTools/__init__.py
+++ PyomoTools/__init__.py
@@ -4,5 +4,6 @@ from .GenerateExpressionString import GenerateExpressionStrings
 from .InfeasibilityReport import InfeasibilityReport
 from .AssertPyomoModelsEqual import AssertPyomoModelsEqual
 from .Solvers import DefaultSolver
+from .MergeModels import MergeModels
 
-__all__ = ['LoadIndexedSet','Load2DIndexedSet','GenerateExpressionStrings','InfeasibilityReport','AssertPyomoModelsEqual',DefaultSolver]
+__all__ = ['LoadIndexedSet','Load2DIndexedSet','GenerateExpressionStrings','InfeasibilityReport','AssertPyomoModelsEqual','DefaultSolver','MergeModels']
