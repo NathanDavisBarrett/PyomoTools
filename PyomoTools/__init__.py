aedf2e4e3a40d14bcac35425e391395f4a91baf9
Nathan Davis Barrett <76917233+NathanDavisBarrett@users.noreply.github.com>
2024-11-21 14:03:51 -0500

    Removed Unnececary Sub-packages.

--- PyomoTools/__init__.py
+++ PyomoTools/__init__.py
@@ -4,9 +4,9 @@ from .GenerateExpressionString import GenerateExpressionStrings
 from .InfeasibilityReport import InfeasibilityReport
 from .AssertPyomoModelsEqual import AssertPyomoModelsEqual
 from .Solvers import DefaultSolver
-from .MergeableModel import MergableModel   
+# from .MergeableModel import MergableModel   
 from .FindLeastInfeasibleSolution import FindLeastInfeasibleSolution
-from .VectorRepresentation.VectorRepresentation import VectorRepresentation
-from .Polytope import Polytope
+# from .VectorRepresentation.VectorRepresentation import VectorRepresentation
+# from .Polytope import Polytope
 
 __all__ = ['LoadIndexedSet','Load2DIndexedSet','GenerateExpressionStrings','InfeasibilityReport','AssertPyomoModelsEqual','DefaultSolver','MergableModel','FindLeastInfeasibleSolution','VectorRepresentation','Polytope']
