5ab7c531d67a722e2283ccd60915258bc508bd81
Nathan Davis Barrett <76917233+NathanDavisBarrett@users.noreply.github.com>
2025-07-01 21:35:56 -0400

    Fixed path to new libraries

--- PyomoTools/kernel/Formulations/__init__.py
+++ PyomoTools/kernel/Formulations/__init__.py
@@ -1,5 +1,9 @@
 from .DoubleSidedBigM import DoubleSidedBigM
 from .MinOperator import MinOperator
 from .MaxOperator import MaxOperator
+from .ConditionalGeq import ConditionalGeq
+from .ConditionalLeq import ConditionalLeq
+from .ConstrainContiguousBinarySeries import ConstrainContiguousBinarySeries
 
-__all__ = ['DoubleSidedBigM','MinOperator','MaxOperator']
+__all__ = ['DoubleSidedBigM','MinOperator','MaxOperator','ConditionalGeq',
+           'ConditionalLeq','ConstrainContiguousBinarySeries']
