5260a59934425f94967ed2653682772f36b6299f
Nathan Davis Barrett <76917233+NathanDavisBarrett@users.noreply.github.com>
2025-07-15 15:57:48 -0400

    Corrected __init__ to collect And and Or Operators

--- PyomoTools/kernel/Formulations/__init__.py
+++ PyomoTools/kernel/Formulations/__init__.py
@@ -4,6 +4,9 @@ from .MaxOperator import MaxOperator
 from .ConditionalGeq import ConditionalGeq
 from .ConditionalLeq import ConditionalLeq
 from .ConstrainContiguousBinarySeries import ConstrainContiguousBinarySeries
+from .AndOperator import AndOperator
+from .OrOperator import OrOperator
 
 __all__ = ['DoubleSidedBigM','MinOperator','MaxOperator','ConditionalGeq',
-           'ConditionalLeq','ConstrainContiguousBinarySeries']
+           'ConditionalLeq','ConstrainContiguousBinarySeries','AndOperator',
+           'OrOperator']
