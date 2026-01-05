6c76a565115c7b9ad7e3b27828b8d78249fef1aa
Nathan Davis Barrett <76917233+NathanDavisBarrett@users.noreply.github.com>
2026-01-05 07:29:12 -0500

    Fixed kernel-level import for RelaxIntegerVarsKernel

--- PyomoTools/kernel/__init__.py
+++ PyomoTools/kernel/__init__.py
@@ -4,6 +4,7 @@ from .InfeasibilityReport import InfeasibilityReport
 from .InfeasibilityReport_Interactive import InfeasibilityReport_Interactive
 from .UnboundedReport import UnboundedReport
 from .AssertPyomoModelsEqual import AssertPyomoModelsEqual
+from .RelaxIntegerVarsKernel import RelaxIntegerVarsKernel
 
 __all__ = [
     "GenerateExpressionStrings",
@@ -11,4 +12,5 @@ __all__ = [
     "InfeasibilityReport_Interactive",
     "UnboundedReport",
     "AssertPyomoModelsEqual",
+    "RelaxIntegerVarsKernel",
 ]
