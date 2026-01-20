75a94f5fe2ded4b5435252271d7ea54af52386ef
Nathan Davis Barrett <76917233+NathanDavisBarrett@users.noreply.github.com>
2026-01-20 12:26:50 -0500

    Fixed import statements

--- PyomoTools/kernel/Dual/__init__.py
+++ PyomoTools/kernel/Dual/__init__.py
@@ -1,3 +1,4 @@
 from .IncorporateDuals import IncorporateDuals
+from .Generate_MIP_Duals import Generate_MIP_Duals
 
-__all__ = ["IncorporateDuals"]
+__all__ = ["IncorporateDuals", "Generate_MIP_Duals"]
